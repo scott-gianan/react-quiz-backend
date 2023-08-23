424988c4acf702cfa1871b6eb6a35917bb6be4e1
scott-gianan <scott.gianan@gmail.com>
2023-08-23 13:57:31 +0800

    connected to db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const PORT = 3000;
 
@@ -7,6 +8,14 @@ app.get("/", (request, response) => {
   response.send("Hello Node API");
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://scott-admin:scotty123@react-quiz.ddxtxqj.mongodb.net/React-Quiz-Bank?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log(`connected to the database`);
+    app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
